e128674a8fad9008830250fbf5f1b41eaa1c41f8
fisher060 <65745653+fisher060@users.noreply.github.com>
2023-10-15 19:59:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,8 +9,5 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-cd openwrt/feeds/luci/themes/
-rm -rf luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
